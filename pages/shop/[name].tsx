6b80ac179bae86b2f3ec411ab74a5926649ac132
Joyzyy <zavoianugabriel70@icloud.com>
2022-10-15 20:03:38 +0300

    asd

--- pages/shop/[name].tsx
+++ pages/shop/[name].tsx
@@ -12,43 +12,44 @@ import { ProductName } from '@/components/Shop/[name]';
 
 const ProductPage: NextPage<ProductResponseIndividual> = ({ error, data }) => {
   return (
-    <ProductContextIndividual.Provider value={{ error, data }}>
-      <ProductName />
-    </ProductContextIndividual.Provider>
+    <></>
+    // <ProductContextIndividual.Provider value={{ error, data }}>
+    //   <ProductName />
+    // </ProductContextIndividual.Provider>
   );
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  // const response = (await axios.get<ProductResponse>(`${process.env.NEXT_PUBLIC_API}/product`))
-  //   .data;
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // const response = (await axios.get<ProductResponse>(`${process.env.NEXT_PUBLIC_API}/product`))
+//   //   .data;
 
-  const data = await prisma.product.findMany();
+//   const data = await prisma.product.findMany();
 
-  const paths = data.map((product) => ({
-    params: {
-      name: product.name.toString(),
-    },
-  }));
+//   const paths = data.map((product) => ({
+//     params: {
+//       name: product.name.toString(),
+//     },
+//   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const response = (
-    await axios.get<ProductResponseIndividual>(
-      `https://nextsedge-di84.vercel.app/api/product/${params?.name}`,
-    )
-  ).data;
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const response = (
+//     await axios.get<ProductResponseIndividual>(
+//       `https://nextsedge-di84.vercel.app/api/product/${params?.name}`,
+//     )
+//   ).data;
 
-  return {
-    props: {
-      error: response.error,
-      data: response.data,
-    },
-  };
-};
+//   return {
+//     props: {
+//       error: response.error,
+//       data: response.data,
+//     },
+//   };
+// };
 
 export default ProductPage;
