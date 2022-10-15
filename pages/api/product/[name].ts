a68ba5f896aef15cbe2604dfa1387e1f014bd6a7
Joyzyy <zavoianugabriel70@icloud.com>
2022-10-15 20:45:52 +0300

    asdasdasd

--- pages/api/product/[name].ts
+++ pages/api/product/[name].ts
@@ -1,55 +1,55 @@
-//import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import type { NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
-
-export default async function handler(req: NextRequest) {
-  try {
-    // ce dracu fac, oare fac bine? ------ 1
-    const { pathname } = new URL(req.url);
-    const paths = pathname.split('/');
-    const myParam = paths[paths.length - 1];
-
-    const data = await prisma.product.findFirst({
-      where: {
-        name: myParam,
-      },
-    });
-
-    return new Response(JSON.stringify({ data: data, error: null }), {
-      status: 200,
-      headers: {
-        'content-type': 'applicaton/json',
-        'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
-      },
-    });
-  } catch (error: any) {
-    return new Response(error.message);
-  }
-}
-
-// type ReqQuery = {
-//   name: string;
+// export const config = {
+//   runtime: 'experimental-edge',
 // };
 
-// interface ExtendedNextApiRequest extends NextApiRequest {
-//   query: ReqQuery;
+// export default async function handler(req: NextRequest) {
+//   try {
+//     // ce dracu fac, oare fac bine? ------ 1
+//     const { pathname } = new URL(req.url);
+//     const paths = pathname.split('/');
+//     const myParam = paths[paths.length - 1];
+
+//     const data = await prisma.product.findFirst({
+//       where: {
+//         name: myParam,
+//       },
+//     });
+
+//     return new Response(JSON.stringify({ data: data, error: null }), {
+//       status: 200,
+//       headers: {
+//         'content-type': 'applicaton/json',
+//         'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+//       },
+//     });
+//   } catch (error: any) {
+//     return new Response(error.message);
+//   }
 // }
 
-// export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
-//   const { name } = req.query;
+type ReqQuery = {
+  name: string;
+};
 
-//   await prisma.product
-//     .findFirst({ where: { name: name } })
-//     .then((product) => {
-//       product
-//         ? res.json({ data: product, error: null })
-//         : res.json({ data: null, error: 'No product found' });
-//     })
-//     .catch((error) => res.status(500).json({ data: null, error: error.message }));
+interface ExtendedNextApiRequest extends NextApiRequest {
+  query: ReqQuery;
+}
 
-//   return;
-// }
+export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
+  const { name } = req.query;
+
+  await prisma.product
+    .findFirst({ where: { name: name } })
+    .then((product) => {
+      product
+        ? res.json({ data: product, error: null })
+        : res.json({ data: null, error: 'No product found' });
+    })
+    .catch((error) => res.status(500).json({ data: null, error: error.message }));
+
+  return;
+}
--- pages/api/product/index.ts
+++ pages/api/product/index.ts
@@ -1,10 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import type { NextRequest } from 'next/server';
 import type { Product } from '@/lib/product';
 import prisma from '@/lib/prisma';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
+// export const config = {
+//   runtime: 'experimental-edge',
+// };
 
 // async function streamToText(stream: ReadableStream<Uint8Array>): Promise<string> {
 //   let result = '';
@@ -19,81 +20,26 @@ export const config = {
 //   return result;
 // }
 
-export default async function handler(req: NextRequest) {
-  if (req.method === 'GET') {
-    try {
-      const data = await prisma.product.findMany();
-      if (!data) return new Response('Couldnt find any products');
-
-      return new Response(JSON.stringify({ data: data, error: null }), {
-        status: 200,
-        headers: {
-          'content-type': 'applicaton/json',
-          'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
-        },
-      });
-    } catch (error: any) {
-      return new Response(JSON.stringify(error.message));
-    }
-  } else if (req.method === 'POST') {
-    try {
-      const body: Product = await req.json();
-      const data = await prisma.product.create({
-        data: {
-          name: body.name,
-          description: body.description,
-          category: body.category,
-          price: body.price,
-          image: body.image,
-          sizes: body.sizes,
-          tabtype: body.tabtype,
-        },
-      });
-
-      if (!data)
-        return new Response(
-          JSON.stringify({ product: null, error: 'Couldnt create a new product' }),
-        );
-
-      return new Response(
-        JSON.stringify({
-          product: data,
-          error: null,
-        }),
-        {
-          status: 200,
-          headers: {
-            'content-type': 'applicaton/json',
-          },
-        },
-      );
-    } catch (error: any) {
-      return new Response(JSON.stringify({ error: error.message }));
-    }
-  }
-}
-
-// interface ExtendedNextApiRequest extends NextApiRequest {
-//   body: Product;
-// }
-
-// export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
+// export default async function handler(req: NextRequest) {
 //   if (req.method === 'GET') {
-//     await prisma.product
-//       .findMany()
-//       .then((product) => {
-//         product
-//           ? res.json({ data: product, error: null })
-//           : res.json({ data: null, error: 'Products not found.' });
-//       })
-//       .catch((error) => res.status(500).json({ error: error.message }));
+//     try {
+//       const data = await prisma.product.findMany();
+//       if (!data) return new Response('Couldnt find any products');
 
-//     return;
+//       return new Response(JSON.stringify({ data: data, error: null }), {
+//         status: 200,
+//         headers: {
+//           'content-type': 'applicaton/json',
+//           'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+//         },
+//       });
+//     } catch (error: any) {
+//       return new Response(JSON.stringify(error.message));
+//     }
 //   } else if (req.method === 'POST') {
-//     const body = req.body;
-
-//     await prisma.product
-//       .create({
+//     try {
+//       const body: Product = await req.json();
+//       const data = await prisma.product.create({
 //         data: {
 //           name: body.name,
 //           description: body.description,
@@ -103,15 +49,70 @@ export default async function handler(req: NextRequest) {
 //           sizes: body.sizes,
 //           tabtype: body.tabtype,
 //         },
-//       })
-//       .then((product) => {
-//         product
-//           ? res.json({ data: product, error: null })
-//           : res.json({ data: null, error: 'couldnt create a new product' });
-//       })
-//       .catch((error) => res.status(500).json({ error }));
+//       });
 
-//     return;
+//       if (!data)
+//         return new Response(
+//           JSON.stringify({ product: null, error: 'Couldnt create a new product' }),
+//         );
+
+//       return new Response(
+//         JSON.stringify({
+//           product: data,
+//           error: null,
+//         }),
+//         {
+//           status: 200,
+//           headers: {
+//             'content-type': 'applicaton/json',
+//           },
+//         },
+//       );
+//     } catch (error: any) {
+//       return new Response(JSON.stringify({ error: error.message }));
+//     }
 //   }
-//   return;
 // }
+
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: Product;
+}
+
+export default async function handler(req: ExtendedNextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    await prisma.product
+      .findMany()
+      .then((product) => {
+        product
+          ? res.json({ data: product, error: null })
+          : res.json({ data: null, error: 'Products not found.' });
+      })
+      .catch((error) => res.status(500).json({ error: error.message }));
+
+    return;
+  } else if (req.method === 'POST') {
+    const body = req.body;
+
+    await prisma.product
+      .create({
+        data: {
+          name: body.name,
+          description: body.description,
+          category: body.category,
+          price: body.price,
+          image: body.image,
+          sizes: body.sizes,
+          tabtype: body.tabtype,
+        },
+      })
+      .then((product) => {
+        product
+          ? res.json({ data: product, error: null })
+          : res.json({ data: null, error: 'couldnt create a new product' });
+      })
+      .catch((error) => res.status(500).json({ error }));
+
+    return;
+  }
+  return;
+}
